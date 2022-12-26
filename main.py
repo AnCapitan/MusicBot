774bb2053577320075cbd7cf992a7b1dbacc91ac
Yury Antsiferov <AnWorkJob@gmail.com>
2022-12-26 16:50:58 +0700

    fix bug

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+from moviepy.video.io.VideoFileClip import VideoFileClip
 from pytube import YouTube, Search
 from ShazamAPI import Shazam
 import telebot
@@ -9,19 +10,20 @@ logging.basicConfig(level=logging.INFO)
 bot = telebot.TeleBot(API_TOKEN, parse_mode=None)
 PWD = ''
 URL_YOUTUBE = 'https://www.youtube.com/watch?v='
+SAVE_DIR = 'data/shazam/'
 
 
 def youtube_download(request_user, message):
     try:
         search = [request_user[i] for i in range(len(request_user))]
         check = str(Search(search).results[0]).replace('<pytube.__main__.YouTube object: videoId=', '').replace('>', '')
-        yt = YouTube(f'{URL_YOUTUBE}{check}')
-        stream = yt.streams.filter(only_audio=True).get_by_itag(140)
-        input_file = str(stream.download(output_path=f'data/{message.chat.id}/')).replace(PWD, '')
-        bot.send_audio(message.chat.id, open(f'{input_file}', 'rb'))
-        os.remove(input_file)
+        request = YouTube(f'{URL_YOUTUBE}{check}')
+        stream = request.streams.filter(only_audio=True).get_by_itag(140)
+        file_music = str(stream.download(output_path=f'data/{message.chat.id}/')).replace(PWD, '')
+        bot.send_audio(message.chat.id, open(f'{file_music}', 'rb'))
+        os.remove(file_music)
     except:
-        bot.send_message(message.chat.id, f'Что то пошло не так, попробуйте снова yo', parse_mode='html')
+        bot.send_message(message.chat.id, f'Что то пошло не так, попробуйте снова', parse_mode='html')
 
 
 @bot.message_handler(commands=['music'])
@@ -37,7 +39,7 @@ def search_music(message):
         request_user = message.text.replace('/music', '').split()
         youtube_download(request_user, message)
     except:
-        bot.send_message(message.chat.id, f'Что то пошло не так, попробуйте снова se', parse_mode='html')
+        bot.send_message(message.chat.id, f'Что то пошло не так, попробуйте снова', parse_mode='html')
 
 
 @bot.message_handler(content_types=['video', 'audio'])
@@ -47,28 +49,27 @@ def search_shazam(message):
         file_name = message.video.file_name
         file_id_info = bot.get_file(message.video.file_id)
         downloaded_file = bot.download_file(file_id_info.file_path)
-        save_dir = 'data/shazam'
-        with open(save_dir + "/" + file_name, 'wb') as new_file:
+        with open(SAVE_DIR + "/" + file_name, 'wb') as new_file:
             new_file.write(downloaded_file)
-        mp4_file_content_to_recognize = open(f'data/shazam/{file_name}', 'rb').read()
+        video = VideoFileClip(os.path.join(f'{SAVE_DIR}/{file_name}'))
+        video.audio.write_audiofile(os.path.join(f'{SAVE_DIR}/{file_name.replace("MP4", "mp3")}'))
+        os.remove(f'{SAVE_DIR}{file_name}')
+        file_name = file_name.replace('MP4', 'mp3')
+        mp4_file_content_to_recognize = open(f'{SAVE_DIR}{file_name}', 'rb').read()
         shazam = Shazam(
             mp4_file_content_to_recognize,
         )
         recognize_generator = shazam.recognizeSong()
-        check = next(recognize_generator)
-        result = f"{check[1]['track']['title']} {check[1]['track']['subtitle']}"
-        os.remove(f'data/shazam/{file_name}')
+        file_shazam = next(recognize_generator)
+        result = f"{file_shazam[1]['track']['title']} {file_shazam[1]['track']['subtitle']}"
+        os.remove(f'{SAVE_DIR}{file_name}')
         bot.send_message(message.chat.id, result, parse_mode='html')
         result = result.split()
         youtube_download(result, message)
     except:
-        bot.send_message(message.chat.id, f'Что то пошло не так, попробуйте снова se', parse_mode='html')
+        os.remove(f'{SAVE_DIR}{file_name}')
+        bot.send_message(message.chat.id, f'Что то пошло не так, попробуйте снова', parse_mode='html')
 
 
 if __name__ == '__main__':
-    try:
-        os.mkdir(f'data/shazam')
-    except:
-        pass
     bot.infinity_polling()
-
